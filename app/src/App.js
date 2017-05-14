df154fe3e44ed6241e86543999d4846d5bf71295
santiaago <santiago.ariassar@gmail.com>
2017-05-14 13:25:04 +0100

    frontend

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>python, javascript, react demo</h2>
+        </div>
+        <p className="App-intro">
+            hello, world
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
